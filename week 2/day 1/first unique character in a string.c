2f8110184235c16a5865fdb1fb92cce7caf4bddc
ananyasingh2024-pixel <ananya.singh2024@glbajajgroup.org>
2026-06-08 12:28:51 +0530

    Implement function to find first unique character

---
+++ week 2/day 1/first unique character in a string.c
@@ -0,0 +1,15 @@
+int firstUniqChar(char* s) {
+    int count[26] = {0};
+    int i;
+
+    for (i = 0; s[i] != '\0';i++) {
+        count[s[i] -'a']++;
+    }
+    
+    for (i = 0; s[i] !='\0'; i++) {
+        if (count[s[i] - 'a'] == 1) {
+            return i;
+        }
+    }
+    return -1;
+}

a3a5e41d074a8cc27ad5d9c20d60e6b1f24f31e5
ananyasingh2024-pixel <ananya.singh2024@glbajajgroup.org>
2026-06-19 10:49:53 +0530

    Implement RecentCounter for tracking recent calls

---
+++ week 3/day 5/Number of Recent Calls.c
@@ -0,0 +1,26 @@
+typedef struct {
+    int q[10005];
+    int front;
+    int rear;
+} RecentCounter;
+
+RecentCounter* recentCounterCreate() {
+    RecentCounter* obj = (RecentCounter*)malloc(sizeof(RecentCounter));
+    obj->front = 0;
+    obj->rear = -1;
+    return obj;
+}
+
+int recentCounterPing(RecentCounter* obj, int t) {
+    obj->q[++obj->rear] = t;
+
+    while (obj->q[obj->front] < t - 3000) {
+        obj->front++;
+    }
+
+    return obj->rear - obj->front + 1;
+}
+
+void recentCounterFree(RecentCounter* obj) {
+    free(obj);
+}

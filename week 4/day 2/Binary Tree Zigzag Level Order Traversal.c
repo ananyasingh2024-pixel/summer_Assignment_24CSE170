71485e2b1264cfadc0ed55ebd58baf718a36236a
ananyasingh2024-pixel <ananya.singh2024@glbajajgroup.org>
2026-06-23 17:10:56 +0530

    Implement zigzag level order traversal for binary tree

---
+++ week 4/day 2/Binary Tree Zigzag Level Order Traversal.c
@@ -0,0 +1,40 @@
+int** zigzagLevelOrder(struct TreeNode* root, int* returnSize, int** returnColumnSizes) {
+    if (root == NULL) {
+        *returnSize = 0;
+        *returnColumnSizes = NULL;
+        return NULL;
+    }
+
+    struct TreeNode* queue[2000];
+    int front = 0, rear = 0;
+    queue[rear++] = root;
+
+    int** result = (int**)malloc(2000 * sizeof(int*));
+    *returnColumnSizes = (int*)malloc(2000 * sizeof(int));
+    *returnSize = 0;
+
+    int leftToRight = 1;
+
+    while (front < rear) {
+        int size = rear - front;
+        result[*returnSize] = (int*)malloc(size * sizeof(int));
+        (*returnColumnSizes)[*returnSize] = size;
+
+        for (int i = 0; i < size; i++) {
+            struct TreeNode* node = queue[front++];
+
+            int index = leftToRight ? i : (size - 1 - i);
+            result[*returnSize][index] = node->val;
+
+            if (node->left)
+                queue[rear++] = node->left;
+            if (node->right)
+                queue[rear++] = node->right;
+        }
+
+        leftToRight = !leftToRight;
+        (*returnSize)++;
+    }
+
+    return result;
+}
